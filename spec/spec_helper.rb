36ef2f1282e5f1b38d1d9e544cfa3b3f7e45a837
olliebaum <oliver@baums.org.uk>
2018-11-05 13:53:38 +0000

    Initialise gemfile and rspec, add simplecov gem

---
+++ spec/spec_helper.rb
@@ -0,0 +1,10 @@
+require 'simplecov'
+
+# SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+#   SimpleCov::Formatter::Console,
+#   # SimpleCov::Formatter::HTMLFormatter
+# ])
+SimpleCov.start
+
+RSpec.configure do |config|
+end
