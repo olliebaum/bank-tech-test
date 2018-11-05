2c4c286965c220b7bf567e3a1ce3cc2f32464def
olliebaum <oliver@baums.org.uk>
2018-11-05 14:33:12 +0000

    Add #deposit method to Account

--- lib/account.rb
+++ lib/account.rb
@@ -1,6 +1,11 @@
 class Account
   attr_reader :balance
+
   def initialize
     @balance = 0
+  end
+
+  def deposit(sum)
+    @balance += sum
   end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -4,4 +4,10 @@ describe Account do
   it 'has a starting balance of 0' do
     expect(subject.balance).to eq 0
   end
+  context '#deposit' do
+    it 'can add money to the account' do
+      subject.deposit(100)
+      expect(subject.balance).to eq 100
+    end
+  end
 end
