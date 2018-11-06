6f37de9a15fba02591cd402575784558f4442e89
olliebaum <oliver@baums.org.uk>
2018-11-06 17:50:30 +0000

    Statement displays numbers to 2 decimal places

--- lib/transaction_history.rb
+++ lib/transaction_history.rb
@@ -8,8 +8,8 @@ class TransactionHistory
   def add_transaction(credit = nil, debit = nil, balance)
     formatted_date = Date.today.strftime("%d/%m/%Y")
     @list << { date: formatted_date,
-             credit: credit,
-              debit: debit,
-            balance: balance }
+             credit: credit != nil ? "%.2f" % credit : nil,
+              debit: debit != nil ? "%.2f" % debit : nil,
+            balance: "%.2f" % balance }
   end
 end
--- spec/transaction_history_spec.rb
+++ spec/transaction_history_spec.rb
@@ -4,11 +4,11 @@ require 'timecop'
 describe TransactionHistory do
   context '#add_transaction' do
     before { Timecop.freeze(Date.today) }
-    
+
     it 'stores a transaction' do
       date1 = Date.today.strftime("%d/%m/%Y")
       subject.add_transaction(100, nil, 100)
-      expect(subject.list).to eq [{ date: date1, credit: 100, debit: nil, balance: 100 }]
+      expect(subject.list).to eq [{ date: date1, credit: "100.00", debit: nil, balance: "100.00" }]
     end
   end
 end
